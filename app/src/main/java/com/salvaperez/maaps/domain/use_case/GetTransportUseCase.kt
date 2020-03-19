8c47a66d3512a7eb134f0085dfda31c353f9bc5b
Salva <salvador.perez@gft.com>
2020-03-19 17:20:33 +0100

    "rename variable"

--- app/src/main/java/com/salvaperez/maaps/domain/use_case/GetTransportUseCase.kt
+++ app/src/main/java/com/salvaperez/maaps/domain/use_case/GetTransportUseCase.kt
@@ -4,14 +4,14 @@ import com.salvaperez.maaps.data.api.fold
 import com.salvaperez.maaps.domain.repository.TransportRepository
 import com.salvaperez.maaps.domain.model.TransportsModel
 
-class GetTransportUseCase(private val rankingRepository: TransportRepository) {
+class GetTransportUseCase(private val transportRepository: TransportRepository) {
 
     suspend operator fun invoke(
         onGetTransportSuccess: (data: List<TransportsModel>?) -> Unit,
         onGetErrorTransport: () -> Unit
     ) {
 
-        rankingRepository.getTransports().fold(
+        transportRepository.getTransports().fold(
             failure = { onGetErrorTransport() },
             success = { data -> onGetTransportSuccess(data) }
         )
